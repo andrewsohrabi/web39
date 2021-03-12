1b302cde97df20b3f90ec7f87d0120af86fc2e1c
andrewsohrabi <andrew.sohrabi@gmail.com>
2021-03-12 01:49:44 -0800

    wip

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
+require('dotenv').config()
+
 console.log('web 39')
+
+const express = require('express')
+
+const server = express()
+server.use(express.json())
+
+console.log('environment:',process.env.NODE_ENV)
+
+if (process.env.NODE_ENV === 'development') { // on Heroku machine, an env variable is called "NODE_ENV" --< 'production'
+    const cors = require('cors')
+    server.use(cors())
+}
+
+
