733c8459668b8e39f5e819d4e489ede6b2fac2d7
andrewsohrabi <andrew.sohrabi@gmail.com>
2021-03-12 01:54:23 -0800

    wip

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 require('dotenv').config()
 
-console.log('web 39')
+// console.log('web 39')
 
 const express = require('express')
 
 const server = express()
 server.use(express.json())
 
-console.log('environment:',process.env.NODE_ENV)
+console.log('env:',process.env.NODE_ENV)
 
 if (process.env.NODE_ENV === 'development') { // on Heroku machine, an env variable is called "NODE_ENV" --< 'production'
     const cors = require('cors')
     server.use(cors())
 }
 
+const PORT = process.env.PORT || 4000
 
+server.listen(PORT, () => {
+    console.log(`listening on ${PORT}`)
+})
